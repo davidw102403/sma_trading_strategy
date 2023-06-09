5d606ef0011f1d676c82cb27387b210d76232f68
davidw102403 <dwang102403@gmail.com>
2023-06-09 13:56:54 -0500

    implement algorithmic strategy

--- strategy.py
+++ strategy.py
@@ -16,11 +16,45 @@ data = yf.download('GOOG', start=start, end=end)
 data[f'SMA_{sma1}'] = data['Adj Close'].rolling(window=sma1).mean()
 data[f'SMA_{sma2}'] = data['Adj Close'].rolling(window=sma2).mean()
 
-
+'''
 plt.plot(data['Adj Close'], label = "Share Price", color = 'black')
 plt.plot(data[f'SMA_{sma1}'], label = f"SMA_{sma1}", color = 'orange')
 plt.plot(data[f'SMA_{sma2}'], label = f"SMA_{sma2}", color = 'blue')
 plt.legend(loc = 'upper left')
 plt.show()
+'''
 
-print(plt.style.available)
+# implement algorithmic strategy
+
+buy_signals = []
+sell_signals = []
+buy_state = 0
+
+for i in range(len(data)):
+    if data[f'SMA_{sma1}'].iloc[i] > data[f'SMA_{sma2}'].iloc[i] and buy_state != 1:
+        buy_signals.append(data['Adj Close'].iloc[i])
+        sell_signals.append(float('nan'))
+        buy_state = 1
+
+    elif data[f'SMA_{sma1}'].iloc[i] < data[f'SMA_{sma2}'].iloc[i] and buy_state != -1:
+        buy_signals.append(float('nan'))
+        sell_signals.append(data['Adj Close'].iloc[i])
+        buy_state = -1
+    else:
+        buy_signals.append(float('nan'))
+        sell_signals.append(float('nan'))
+
+
+data['Buy Signals'] = buy_signals
+data['Sell Signals'] = sell_signals
+
+print(data)
+
+plt.plot(data['Adj Close'], label = "Share Price", color = "black", alpha = 0.5)
+plt.plot(data[f'SMA_{sma1}'], label = f"SMA_{sma1}", color = 'orange', linestyle = "--")
+plt.plot(data[f'SMA_{sma2}'], label = f"SMA_{sma2}", color = 'blue', linestyle = "--")
+plt.scatter(data.index, data['Buy Signals'], label = "Buy Signal", marker = "^", color = "green")
+plt.scatter(data.index, data['Sell Signals'], label = "Sell Signal", marker = "v", color = "red")
+plt.legend(loc = "upper left")
+plt.show()
+
